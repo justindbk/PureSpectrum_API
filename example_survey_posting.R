a899dc4b610ca166fa89fdfc3f5d6af28c6b19e0
Justin de Benedictis-Kessner <justindbk@gmail.com>
2021-03-25 16:19:46 -0400

    added better test and live survey URLs with variable pass-thrus

--- example_survey_posting.R
+++ example_survey_posting.R
@@ -28,8 +28,8 @@ ps_post_survey(survey_title = "sample-st.louis-wquotas-nested-wzips",
                survey_length = 10, # length of survey, in minutes
                offer_price = 1.5,
                field_time = 14, # field time in days
-               live_url = "https://my.survey.com?survey_id=1234",
-               test_url = "https://my.survey.com?survey_id=1234&amp;test=1",
+               live_url = "https://harvard.az1.qualtrics.com/jfe/form/SV_5d41NokucVvbGbY?sample_frame=St. Louis&zipcode=[%%229%%]&gender=[%%211%%]&age=[%%212%%]&race=[%%214%%]&educ=[%%216%%]&state=[%%225%%]&inc=[%%213%%]&relat=[%%217%%]&child=[%%218%%]&emp=[%%215%%]",
+               test_url = "https://harvard.az1.qualtrics.com/jfe/preview/SV_5d41NokucVvbGbY?Q_CHL=preview&Q_SurveyVersionID=current&sample_frame=St. Louis&zipcode=[%%229%%]&gender=[%%211%%]&age=[%%212%%]&race=[%%214%%]&educ=[%%216%%]&state=[%%225%%]&inc=[%%213%%]&relat=[%%217%%]&child=[%%218%%]&emp=[%%215%%]",
                quotas = filter(quotas,sample_frame=="St. Louis"),
                zips = c("02138","02139"), # vector list of enquoted zip codes
                access_token = my_token
@@ -43,8 +43,8 @@ ps_post_survey(survey_title = "sample-wquotas-nested-wstate",
                survey_length = 10, # length of survey, in minutes
                offer_price = 1.5,
                field_time = 14, # field time in days
-               live_url = "https://my.survey.com?survey_id=1234",
-               test_url = "https://my.survey.com?survey_id=1234&amp;test=1",
+               live_url = "https://harvard.az1.qualtrics.com/jfe/form/SV_5d41NokucVvbGbY?sample_frame=St. Louis&zipcode=[%%229%%]&gender=[%%211%%]&age=[%%212%%]&race=[%%214%%]&educ=[%%216%%]&state=[%%225%%]&inc=[%%213%%]&relat=[%%217%%]&child=[%%218%%]&emp=[%%215%%]",
+               test_url = "https://harvard.az1.qualtrics.com/jfe/preview/SV_5d41NokucVvbGbY?Q_CHL=preview&Q_SurveyVersionID=current&sample_frame=St. Louis&zipcode=[%%229%%]&gender=[%%211%%]&age=[%%212%%]&race=[%%214%%]&educ=[%%216%%]&state=[%%225%%]&inc=[%%213%%]&relat=[%%217%%]&child=[%%218%%]&emp=[%%215%%]",
                quotas = filter(quotas,sample_frame=="Massachusetts"),
                states = filter(quotas,sample_frame=="Massachusetts") %>% select(state_fips_code) %>% unique() %>% as.character(),
                access_token = my_token
